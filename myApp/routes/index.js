654928313ee0474ea107dc7b65d340d8a5e15ff4
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-12 15:19:01 -0700

    post to database set up

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -5,17 +5,18 @@ var conString = "postgres://@localhost/memoriesapp";
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/api/v1/memories', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query('SELECT * from memories', function(err, result) {
+    client.query('INSERT into memories(old_days, these_days, year) VALUES($1,$2,$3) ', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
       done();
-      res.render('index', {});
+      res.render('index');
       if (err) {
         return console.error('error running query', err);
       }
+      console.log('connected to db')
     });
   });
 });
