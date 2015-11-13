db9c2956e54ebd1401716f24fe6a2a007f0bdd75
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-13 08:57:50 -0700

    trying to fix cors issue

--- myApp/app.js
+++ myApp/app.js
@@ -27,11 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
  
 app.get('http://g12-lisa-carlson-memories.cfapps.io/api/v1/memories', function(req, res, next){
   res.json({msg: 'This is CORS-enabled for all origins!'});
