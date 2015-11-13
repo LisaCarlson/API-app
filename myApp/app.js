a6da69f63b8ec388bcb60e10a42a08a60261f16d
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-12 20:40:02 -0700

    trying to fix cors

--- myApp/app.js
+++ myApp/app.js
@@ -27,7 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
  
 app.get('/api/v1/memories', function(req, res, next){
   res.json({msg: 'This is CORS-enabled for all origins!'});
